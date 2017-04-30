c3c2db8526e981712c64dd6b59b91c9c47d32d85
Rael Kenny <RaelKenny@gmail.com>
2017-04-30 18:57:20 -0400

    initial commit

---
+++ PokeDex/Pokemon.swift
@@ -0,0 +1,34 @@
+//
+//  Pokemon.swift
+//  PokeDex
+//
+//  Created by Rael Kenny on 4/30/17.
+//  Copyright © 2017 Rael Kenny. All rights reserved.
+//
+
+import Foundation
+
+class Pokemon {
+    
+    private var _name: String!
+    private var _pokedexID: Int!
+    
+    //getters    
+    var name:String {
+        return _name
+    }
+    
+    var pokedexID:Int {
+        return _pokedexID
+    }
+    
+    //initializer
+    init(name: String, pokedexID: Int) {
+        self._name = name
+        self._pokedexID = pokedexID
+    }
+    
+
+    
+    
+}
---
+++ PokeDex/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  PokeDex
+//
+//  Created by Rael Kenny on 4/30/17.
+//  Copyright © 2017 Rael Kenny. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let charmander = Pokemon(name: "Charmander", pokedexID: 4)
+        
+    }
+    
+    
+
+
+}
+
